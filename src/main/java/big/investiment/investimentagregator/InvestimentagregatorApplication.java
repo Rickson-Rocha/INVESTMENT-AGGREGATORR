28cd04b495b1a87aece125b47355122e91d017d9
Rickson Rocha <ricksonroccha21@gmail.com>
2024-12-14 16:40:04 -0300

    [init]:first commit

---
+++ src/main/java/big/investiment/investimentagregator/InvestimentagregatorApplication.java
@@ -0,0 +1,13 @@
+package big.investiment.investimentagregator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvestimentagregatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InvestimentagregatorApplication.class, args);
+	}
+
+}
