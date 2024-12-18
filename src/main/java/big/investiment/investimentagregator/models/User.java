f6c539d20ee8ae60f3eb7afcad374989a85f011d
Rickson Rocha <ricksonroccha21@gmail.com>
2024-12-18 10:46:55 -0300

    [feat]: add lombok notations

--- src/main/java/big/investiment/investimentagregator/models/User.java
+++ src/main/java/big/investiment/investimentagregator/models/User.java
@@ -1,21 +1,27 @@
 package big.investiment.investimentagregator.models;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
-import lombok.Getter;
+import lombok.NoArgsConstructor;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
 import java.time.Instant;
+import java.util.List;
 import java.util.UUID;
 
 @Entity
 @Table(name = "tb_user")
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
-    @Column(name = "user_id")
+    @Column(name = "user_id",nullable = false, updatable = false)
     private UUID userId;
 
     @Column(unique = true)
@@ -30,4 +36,7 @@ public class User {
 
     @UpdateTimestamp
     private Instant updatedTimeStamp;
+
+    @OneToMany(mappedBy = "user")
+    private List<Account> accounts;
 }
